4d8ed91bbc704634b11de017eb787f9cf8c1c76d
Stellareign <tuguran@mail.ru>
2022-11-15 20:36:03 +0300

    Задачи 3

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Домашка 10 -  Строки. Задача 1:");
-        String firstName = "Ivan ";
-        String middleNAme = "Ivanovich ";
-        String lastName = "Ivanov ";
-        String fullName = lastName + firstName + middleNAme;
+        System.out.println("Домашка 10 -  Строки. Задача 3:");
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
         System.out.println(fullName);
-        System.out.println("" +
-                "");
-        System.out.println("Домашка 10 -  Строки. Задача 2:");
-        String fullName1 = fullName.toUpperCase();
-        System.out.println("Данные ФИО сотрудника для заполнения отчёта - " + fullName1);
     }
 }
