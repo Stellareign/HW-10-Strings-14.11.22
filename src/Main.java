1bd2d7af8d11e3ad24814a80d207a55094c9bd32
Stellareign <tuguran@mail.ru>
2022-11-15 20:38:43 +0300

    Задачи 3

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.println("Домашка 10 -  Строки. Задача 3:");
         String fullName = "Иванов Семён Семёнович";
         fullName = fullName.replace("ё", "е");
-        System.out.println(fullName);
+        System.out.println("Данные ФИО сотрудника - " + fullName); // Эта домашка неприлично лёгкая. Передышка после массивов :)
     }
 }
